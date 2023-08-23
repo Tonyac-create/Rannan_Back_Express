ac9ad64e7f36d66d4cc1fb173fbe1729018a2925
Julien Le Brenn <lebrennj@gmail.com>
2023-08-23 15:47:27 +0200

    add GroupService && factorisation GroupController

--- src/controller/GroupController.ts
+++ src/controller/GroupController.ts
@@ -9,26 +9,19 @@ export class GroupController {
     private groupService = new GroupService()
 
 //Get all groups
-//////////////////////A MODIFIER POUR RENVOYER UNIQUEMENT LE CREATOR_ID/////////////////////////
     async all(request: Request, response: Response, next: NextFunction) {
 
         const groups = await this.groupService.allWithCreator()
-        // const groups = await this.groupRepository.find({
-        //     relations: ['creator'] // Specify the name of the relation to include
-        // });
 
         return response.json(groups);
     }
 
 //Get group by creator id
     async groupsByCreatorId(request: Request, response: Response, next: NextFunction) {
+
         const creatorId = parseInt(request.params.id);
-        //user = get user from CreatorId
-        //if(!user ){//je sors de la fonction}
-        const groups = await this.groupRepository.find({
-            where: { creator: { id: creatorId } },
-            relations: ['creator'] // Optionally include creator details
-        });
+
+        const groups = await this.groupService.allByCreatorId(creatorId)
 
         if (!groups || groups.length === 0) {
             return response.status(404).json({ error: "No groups found for the specified creator" });
@@ -40,55 +33,48 @@ export class GroupController {
 
 
 //Post new group
+
     async save(request: Request, response: Response, next: NextFunction) {
         const { name, creatorId } = request.body;
 
-        const group = this.groupRepository.create({
-            name,
-            creator: { id: creatorId } // Crée une instance User avec seulement l'ID du créateur
-        });
+        try {
+            const savedGroup = await this.groupService.saveGroup(name, creatorId);
+            return response.status(201).json(savedGroup);
+        } catch (error) {
+            // Gérer les erreurs, par exemple en renvoyant une réponse d'erreur appropriée
+            console.error("Error while saving the group:", error); // Log the actual error
 
-        const savedGroup = await this.groupRepository.save(group);
-
-        return response.status(201).json(savedGroup);
+            return response.status(500).json({ error: "An error occurred while saving the group" });
+        }
     }
 
 
-//Update group
+// Update group
     async update(request: Request, response: Response, next: NextFunction) {
         const id = parseInt(request.params.id);
         const { name, limited_at } = request.body;
 
-        // Find the group to update
-        const groupToUpdate = await this.groupRepository.findOneBy({ id })
-
-        if (!groupToUpdate) {
-            return response.status(404).json({ error: "Group not found" });
+        try {
+            const updatedGroup = await this.groupService.updateGroup(id, name, limited_at);
+            return response.json(updatedGroup);
+        } catch (error) {
+            console.error("Error while updating the group:", error); // Log the actual error
+            return response.status(500).json({ error: "An error occurred while updating the group" });
         }
-
-        // Update fields
-        groupToUpdate.name = name;
-        groupToUpdate.limited_at = limited_at;
-
-        // Save the changes
-        const updatedGroup = await this.groupRepository.save(groupToUpdate);
-
-        return response.json(updatedGroup);
     }
 
-//Delete group
+// Delete group
     async remove(request: Request, response: Response, next: NextFunction) {
-        const id = parseInt(request.params.id)
+        const id = parseInt(request.params.id);
 
-        let groupToRemove = await this.groupRepository.findOneBy({ id })
-
-        if (!groupToRemove) {
-            return "this group not exist"
+        try {
+            const removalResult = await this.groupService.removeGroup(id);
+            return response.json(removalResult);
+        } catch (error) {
+            console.error("Error while removing the group:", error); // Log the actual error
+            return response.status(500).json({ error: "An error occurred while removing the group" });
         }
-
-        await this.groupRepository.remove(groupToRemove)
-
-        return "group has been removed"
     }
+
 
 }
--- src/service/GroupService.ts
+++ src/service/GroupService.ts
@@ -5,21 +5,61 @@ export class GroupService {
 
     private groupRepository = AppDataSource.getRepository(Group)
 
+    //////////////////////A MODIFIER POUR RENVOYER UNIQUEMENT LE CREATOR_ID/////////////////////////
+
     async allWithCreator() : Promise<Group[]>
     {
         const groups = await this.groupRepository.find({
             relations: ['creator'] // Specify the name of the relation to include
         });
-        console.log("groups", groups)
         return groups;
     }
-
-    async allByCreatorId(creatorId :number):Promise<Group[]>{
+    
+    async allByCreatorId(creatorId: number): Promise<Group[]> {
         const groups = await this.groupRepository.find({
             where: { creator: { id: creatorId } },
-            relations: ['creator'] // Optionally include creator details
+            select: ["id", "name"], // Select only the necessary fields
+            relations: ['creator']
         });
         return groups;
+    }
+    
+
+    async saveGroup(name: string, creatorId: number): Promise<Group> {
+
+        const group = this.groupRepository.create({
+            name,
+            creator: { id: creatorId }
+        });
+
+        const savedGroup = await this.groupRepository.save(group);
+        return savedGroup;
+    }
+
+    async updateGroup(id: number, name: string, limited_at: Date | null): Promise<Group | undefined> {
+        const groupToUpdate = await this.groupRepository.findOneBy({ id })
+    
+        if (!groupToUpdate) {
+            throw new Error("Group not found");
+        }
+    
+        groupToUpdate.name = name;
+        groupToUpdate.limited_at = limited_at;
+    
+        const updatedGroup = await this.groupRepository.save(groupToUpdate);
+        return updatedGroup;
+    }
+    
+    async removeGroup(id: number): Promise<string> {
+        const groupToRemove = await this.groupRepository.findOneBy({ id })
+    
+        if (!groupToRemove) {
+            throw new Error("Group not found");
+        }
+    
+        await this.groupRepository.remove(groupToRemove);
+    
+        return "Group has been removed";
     }
 
 }
