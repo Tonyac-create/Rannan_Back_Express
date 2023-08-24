abaaef48212c0e914a0faaaac5513ff8071f333e
Julien Le Brenn <lebrennj@gmail.com>
2023-08-24 10:59:13 +0200

    Add User in Group Ok

--- src/controller/GroupController.ts
+++ src/controller/GroupController.ts
@@ -1,4 +1,3 @@
-// import { AppDataSource } from "../data-source"
 import { NextFunction, Request, Response } from "express"
 import { GroupService } from "../service/groupService"
 export class GroupController {
@@ -31,41 +30,38 @@ export class GroupController {
 
 
 
-// // Add user to a group( A terminer )
-//     async addUserInGroup(request: Request, response: Response, next: NextFunction) {
-//         const userId = parseInt(request.params.id);
-//         const groupId = parseInt(request.body.groupId);
+// Add user to a group
 
-//         if (isNaN(userId) || isNaN(groupId)) {
-//             return response.status(400).json({ error: "Invalid userId or groupId" });
-//         }
-//         console.log("coucou ici express  "+ groupId)
-//         try {
-//             const addedUser = await this.groupService.addUserToGroup(userId, groupId);
-//             return response.status(201).json(addedUser);
-//         } catch (error) {
-//             return response.status(500).json({ error: "An error occurred while adding user to group" });
-//         }
-//     }
+    async addUserInGroup(request: Request, response: Response, next: NextFunction) {
+        const userId = parseInt(request.params.id);
+        const groupId = parseInt(request.body.groupId);
+        try {
+            const addedUser = await this.groupService.addUserToGroup(userId, groupId);
+            return addedUser;
+        } catch (error) {
+            return error
+        }
+    }
 
 //Post new group
 
-async save(request: Request, response: Response, next: NextFunction) {
-    try {
-        const { name, creatorId } = request.body;
-        const savedGroup = await this.groupService.saveGroup(name, creatorId); // Pass the individual arguments
-        return savedGroup; // Return the saved group with a success status
-    } catch (error) {
-        // Gérer les erreurs, par exemple en renvoyant une réponse d'erreur appropriée
-        console.error("Error while saving the group:", error); // Log the actual error
+    async save(request: Request, response: Response, next: NextFunction) { 
+        try {
+            const { name, creatorId } = request.body;
+            const savedGroup = await this.groupService.saveGroup(name, creatorId); // Pass the individual arguments
+            return savedGroup; // Return the saved group with a success status
+        } catch (error) {
+            // Gérer les erreurs, par exemple en renvoyant une réponse d'erreur appropriée
+            console.error("Error while saving the group:", error); // Log the actual error
 
-        return response.status(500).json({ error: "An error occurred while saving the group" });
+            return response.status(500).json({ error: "An error occurred while saving the group" });
+        }
     }
-}
 
 
 
 // Update group
+
     async update(request: Request, response: Response, next: NextFunction) {
         const id = parseInt(request.params.id);
         const { name, limited_at } = request.body;
@@ -80,6 +76,7 @@ async save(request: Request, response: Response, next: NextFunction) {
     }
 
 // Delete group
+
     async remove(request: Request, response: Response, next: NextFunction) {
         const id = parseInt(request.params.id);
 
